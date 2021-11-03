d2d8a89ec38518b695549c0c139654e2a03a39ad
Manuel Rodriguez <mrodriguez@onit.it>
2021-11-03 08:04:05 +0100

    Aggiunta files dopo aver compilato

---
+++ provaBackEnd/Domain/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace provaBackEnd.Domain.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName ="varchar(20)")]
+        public string UserNAme { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string Password { get; set; }
+    }
+}
